2f97a46f15aed620e6e5c8443cfaf37962b50e58
placson <placson@gmail.com>
2022-04-27 16:31:33 -0700

    Add files via upload
    Script to download the entire 57-part series of RC Sproul's teaching on "From Dust to Glory"

---
+++ download_dust_to_glory-by-rc-sproul.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+wget -O "1-Creation.mp3" https://mp3.sermonaudio.com/filearea/1214111044586/1214111044586.mp3
+wget -O "2-The Image of God in Man.mp3" https://mp3.sermonaudio.com/filearea/121611934134/121611934134.mp3
+wget -O "3-The Fall.mp3" https://mp3.sermonaudio.com/filearea/13121012435/13121012435.mp3
+wget -O "4-Covenant with Abraham.mp3" https://mp3.sermonaudio.com/filearea/13121014196/13121014196.mp3
+wget -O "5-Patriarchal Blessing.mp3" https://mp3.sermonaudio.com/filearea/1612172100/1612172100.mp3
+wget -O "6-Moses and the Exodus.mp3" https://mp3.sermonaudio.com/filearea/116121693010/116121693010.mp3
+wget -O "7-The Passover.mp3" https://mp3.sermonaudio.com/filearea/1241284890/1241284890.mp3
+wget -O "8-The Giving of the Law.mp3" https://mp3.sermonaudio.com/filearea/127121138113/127121138113.mp3
+wget -O "9-The Tabernacle.mp3" https://mp3.sermonaudio.com/filearea/26121114182/26121114182.mp3
+wget -O "10-Aaron and the Priesthood.mp3" https://mp3.sermonaudio.com/filearea/21312937452/21312937452.mp3
+wget -O "11-Old Testament Sacrificial System.mp3" https://mp3.sermonaudio.com/filearea/217121140452/217121140452.mp3
+wget -O "12-Joshua and the Conquest of Canaan.mp3" https://mp3.sermonaudio.com/filearea/311218491210/311218491210.mp3
+wget -O "13-The Cycle of Judges.mp3" https://mp3.sermonaudio.com/filearea/32121715498/32121715498.mp3
+wget -O "14-The Monarchy.mp3" https://mp3.sermonaudio.com/filearea/312122210171/312122210171.mp3
+wget -O "15-David.mp3" https://mp3.sermonaudio.com/filearea/3191293535/3191293535.mp3
+wget -O "16-Solomon and the Temple.mp3" https://mp3.sermonaudio.com/filearea/4212944480/4212944480.mp3
+wget -O "17-The Divided Kingdom.mp3" https://mp3.sermonaudio.com/filearea/421294681/421294681.mp3
+wget -O "18-Elijah.mp3" https://mp3.sermonaudio.com/filearea/416121614296/416121614296.mp3
+wget -O "19-Isaiah.mp3" https://mp3.sermonaudio.com/filearea/420121437493/420121437493.mp3
+wget -O "20-Jeremiah.mp3" https://mp3.sermonaudio.com/filearea/430121752569/430121752569.mp3
+wget -O "21-The Exile.mp3" https://mp3.sermonaudio.com/filearea/57121134191/57121134191.mp3
+wget -O "22-Ezekiel.mp3" https://mp3.sermonaudio.com/filearea/514121030147/514121030147.mp3
+wget -O "23-Daniel.mp3" https://mp3.sermonaudio.com/filearea/52112958319/52112958319.mp3
+wget -O "24-Ezra and Nehemiah.mp3" https://mp3.sermonaudio.com/filearea/2251395580/2251395580.mp3
+wget -O "25-Amos and Hosea.mp3" https://mp3.sermonaudio.com/filearea/53112125227/53112125227.mp3
+wget -O "26-Joel, Micah, and Habakkuk.mp3" https://mp3.sermonaudio.com/filearea/61412174023/61412174023.mp3
+wget -O "27-Characteristics of Wisdom Literature.mp3" https://mp3.sermonaudio.com/filearea/61512918230/61512918230.mp3
+wget -O "28-Psalms.mp3" https://mp3.sermonaudio.com/filearea/62612172071/62612172071.mp3
+wget -O "29-Ecclesiastes.mp3" https://mp3.sermonaudio.com/filearea/62912175522/62912175522.mp3
+wget -O "30-Job.mp3" https://mp3.sermonaudio.com/filearea/79121130280/79121130280.mp3
+wget -O "31-The Intertestamental Period.mp3" https://mp3.sermonaudio.com/filearea/717121633560/717121633560.mp3
+wget -O "32-John the Baptist.mp3" https://mp3.sermonaudio.com/filearea/724121017108/724121017108.mp3
+wget -O "33-The Birth of Jesus.mp3" https://mp3.sermonaudio.com/filearea/81121137109/81121137109.mp3
+wget -O "34-The Early Years of Jesus' Life.mp3" https://mp3.sermonaudio.com/filearea/88121251181/88121251181.mp3
+wget -O "35-The Baptism and Temptation of Jesus.mp3" https://mp3.sermonaudio.com/filearea/81112034292/81112034292.mp3
+wget -O "36-Jesus' Inaugural Address and Public Ministry.mp3" https://mp3.sermonaudio.com/filearea/82012180102/82012180102.mp3
+wget -O "37-The Teaching of Jesus: Parables.mp3" https://mp3.sermonaudio.com/filearea/83012918230/83012918230.mp3
+wget -O "38-Interpreting Parables.mp3" https://mp3.sermonaudio.com/filearea/83112122093/83112122093.mp3
+wget -O "39-The Miracles of Jesus.mp3" https://mp3.sermonaudio.com/filearea/910121313573/910121313573.mp3
+wget -O "40-The Caesarea-Philippi Confession.mp3" https://mp3.sermonaudio.com/filearea/919121111322/919121111322.mp3
+wget -O "41-The Transfiguration.mp3" https://mp3.sermonaudio.com/filearea/921121343222/921121343222.mp3
+wget -O "42-The Triumphal Entry.mp3" https://mp3.sermonaudio.com/filearea/1021216987/1021216987.mp3
+wget -O "43-The Cross.mp3" https://mp3.sermonaudio.com/filearea/105121342535/105121342535.mp3
+wget -O "44-The Resurrection.mp3" https://mp3.sermonaudio.com/filearea/1015121049208/1015121049208.mp3
+wget -O "45-The Ascension.mp3" https://mp3.sermonaudio.com/filearea/1020121630325/1020121630325.mp3
+wget -O "46-Pentecost.mp3" https://mp3.sermonaudio.com/filearea/1028121752534/1028121752534.mp3
+wget -O "47-The Expansion of the Church.mp3" https://mp3.sermonaudio.com/filearea/115121224270/115121224270.mp3
+wget -O "48-The Conversion of Paul.mp3" https://mp3.sermonaudio.com/filearea/11912163192/11912163192.mp3
+wget -O "49-Romans.mp3" https://mp3.sermonaudio.com/filearea/1119121518288/1119121518288.mp3
+wget -O "50-I and II Corinthians.mp3" https://mp3.sermonaudio.com/filearea/121121114473/121121114473.mp3
+wget -O "51-Prison Epistles.mp3" https://mp3.sermonaudio.com/filearea/121121116565/121121116565.mp3
+wget -O "52-I and II Timothy.mp3" https://mp3.sermonaudio.com/filearea/1213111400/1213111400.mp3
+wget -O "53-Hebrews.mp3" https://mp3.sermonaudio.com/filearea/12131115171/12131115171.mp3
+wget -O "54-General Epistles.mp3" https://mp3.sermonaudio.com/filearea/12131116180/12131116180.mp3
+wget -O "55-Introduction to Revelation.mp3" https://mp3.sermonaudio.com/filearea/12131117232/12131117232.mp3
+wget -O "56-The Christ of Revelation.mp3" https://mp3.sermonaudio.com/filearea/17131038527/17131038527.mp3
+wget -O "57-The Glory of God.mp3" https://mp3.sermonaudio.com/filearea/11413117441/11413117441.mp3
